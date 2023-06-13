a07b7135bfdb6f61d8d7d283f865668859f2f28c
Siddhartha <sid@countvajhula.com>
2023-06-13 16:59:17 -0700

    send a file and verify its checksum

--- client.py
+++ client.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
 import Pyro5.api
+import os
+from models import Metadata
+from hashlib import sha1
 
-data = input("What data would you like to store? ").strip()
 
-with Pyro5.api.Proxy("PYRONAME:example.peer") as peer:
-    print(peer.store(data))
+DISK = 'disk/client'
+
+
+def main():
+    filename = input("What file would you like to store? ").strip()
+
+    with Pyro5.api.Proxy("PYRONAME:example.peer") as peer:
+        with open(os.path.join(DISK, filename), 'rb') as f:
+            contents = f.read()
+            checksum = sha1()
+            checksum.update(contents)
+            m = Metadata(filename, checksum.hexdigest())
+            print(peer.store(m.__dict__, contents))
+
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python
 import Pyro5.api
+import os
+import base64
+from models import Metadata
+from hashlib import sha1
+
+
+DISK = 'disk/server'
+
 
 @Pyro5.api.expose
-class Peer(object):
-    def store(self, name):
-        return f"Hello! I've saved {name}.\n"
+class Peer:
+    def store(self, metadata, contents):
+        m = Metadata(**metadata)
+        with open(os.path.join(DISK, m.filename), 'wb') as f:
+            decoded = base64.decodebytes(bytes(contents['data'], 'utf-8'))
+            checksum = sha1()
+            checksum.update(decoded)
+            assert checksum.hexdigest() == m.checksum
+            return f"Hello! I've saved {m.filename}.\n"
 
 
-daemon = Pyro5.api.Daemon()             # make a Pyro daemon
-uri = daemon.register(Peer)             # register the peer as a Pyro object
-ns = Pyro5.api.locate_ns()              # find the name server
-ns.register("example.peer", uri)        # register the object with a name in the name server
+def main():
+    daemon = Pyro5.api.Daemon()             # make a Pyro daemon
+    uri = daemon.register(Peer)             # register the peer as a Pyro object
+    ns = Pyro5.api.locate_ns()              # find the name server
+    ns.register("example.peer", uri)        # register the object with a name in the name server
+    print("Ready.")
+    daemon.requestLoop()                    # start the event loop of the server to wait for calls
 
-print("Ready.")
-daemon.requestLoop()                    # start the event loop of the server to wait for calls
+
+if __name__ == '__main__':
+    main()
