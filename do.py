16e5a8af973fc4d42b60972797a013a52453a26c
Siddhartha <sid@countvajhula.com>
2023-06-19 12:56:27 -0700

    add a help menu to the `do` script

--- do.py
+++ do.py
@@ -77,8 +77,23 @@ def get_file():
         peer.request_to_get(filename)
 
 
+def show_help():
+    info = ["[d] dir - show files stored on a peer",
+            "[s] Store a file - instruct a peer to request to store a file on the network",
+            "[x] Delete a file - remove a file from a specific peer",
+            "[g] get a file - instruct a peer to request a file stored on the network",
+            "[?] help - show this menu",
+            "[q] quit - exit to the shell"]
+    print("\n".join(info))
+
+
 def main_menu():
-    options = ["[d] dir", "[s] Store a file", "[x] Delete a file", "[g] get a file", "[q] quit"]
+    options = ["[d] dir",
+               "[s] Store a file",
+               "[x] Delete a file",
+               "[g] get a file",
+               "[?] help",
+               "[q] quit"]
     menu = TerminalMenu(options, title="What would you like to do?")
     selected_index = menu.show()
     if selected_index == 0:
@@ -89,6 +104,10 @@ def main_menu():
         delete_file()
     elif selected_index == 3:
         get_file()
+    elif selected_index == 4:
+        show_help()
+    elif selected_index == 5:
+        exit(0)
     else:
         exit(0)
 
