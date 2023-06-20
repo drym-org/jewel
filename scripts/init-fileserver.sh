7a83b70c4140d350186dcf355c215c4027a222e1
Siddhartha <sid@countvajhula.com>
2023-06-20 13:28:55 -0700

    update paths in init and run scripts

--- scripts/init-fileserver.sh
+++ scripts/init-fileserver.sh
@@ -5,14 +5,4 @@
 # and the paths here are relative to that root
 
 mkdir -p live/fileserver/disk
-
-cp fileserver.py live/fileserver
-cp models.py live/fileserver
-cp names.py live/fileserver
-cp checksum.py live/fileserver
-cp networking.py live/fileserver
-cp log.py live/fileserver
-cp file.py live/fileserver
-cp config.py live/fileserver
-cp striping.py live/fileserver
-cp -R scheme live/$1
+cp -R jewel live/fileserver
--- scripts/init-peer.sh
+++ scripts/init-peer.sh
@@ -5,17 +5,6 @@
 # and the paths here are relative to that root
 
 # TODO: different files on different peers
-# TODO: copy all py files to a src folder so they can be copied over
-# wholesale
 
 mkdir -p live/$1/disk
-cp peer.py live/$1
-cp models.py live/$1
-cp names.py live/$1
-cp checksum.py live/$1
-cp networking.py live/$1
-cp log.py live/$1
-cp file.py live/$1
-cp config.py live/$1
-cp striping.py live/$1
-cp -R scheme live/$1
+cp -R jewel live/$1
