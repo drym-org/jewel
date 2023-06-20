384c65f11983003751e8d60a861c01fe820e74c5
Siddhartha <sid@countvajhula.com>
2023-06-20 13:08:41 -0700

    on second thought, don't use subcommands and obscure the bash scripts

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ First, ensure that you are at the root path of the cloned ``jewel`` repo.
 ## In one terminal window
 
 ``` shell
-$ jewel init [N]
+$ ./init.sh [N]
 ```
 
 This initializes the network (the fileserver and peers) by copying all of the necessary files to the ``live`` folder, overwriting any local changes. The argument ``N`` indicates how many peers you want.
@@ -21,7 +21,7 @@ This initializes the network (the fileserver and peers) by copying all of the ne
 Now spin up your network:
 
 ``` shell
-$ jewel run [N]
+$ ./run.sh [N]
 ```
 
 This starts the nameserver, fileserver and peers. The ``N`` argument indicates how many of the peers you'd like to spin up.
@@ -29,14 +29,14 @@ This starts the nameserver, fileserver and peers. The ``N`` argument indicates h
 ## In another terminal window
 
 ``` shell
-$ jewel do
+$ jewel
 ```
 
 This allows you to do things like have one peer store a file on another.
 
 ## Looking at things
 
-All logs go to the first terminal window, the one where you ran ``jewel run``.
+All logs go to the first terminal window, the one where you ran ``run.sh``.
 
 You can also ask the nameserver about live nodes on the network:
 
@@ -50,7 +50,7 @@ You can inspect the files stored in each peer by looking in the ``disk`` folder 
 
 ## The Network
 
-There are two main python modules -- ``peer.py`` and ``fileserver.py``. When deployed using ``jewel init``, these modules are placed in dedicated folders to model distinct nodes or machines on a network. These folders also each contain a ``disk`` subfolder that represents the node's hard drive.
+There are two main python modules -- ``peer.py`` and ``fileserver.py``. When deployed using ``init.sh``, these modules are placed in dedicated folders to model distinct nodes or machines on a network. These folders also each contain a ``disk`` subfolder that represents the node's hard drive.
 
 Both of these modules use Pyro5 to interact over the network. This library allows us to do simple method invocations on remote objects -- a really seamless RPC framework, basically.
 
@@ -58,11 +58,11 @@ Besides these two modules, everything else is just supporting modules.
 
 ## The Interface
 
-The ``do.py`` module is the user interface to this whole thing. It uses a nifty library called ``simple-term-menu``, and the selected options appear in the code as indexes that we can use to take appropriate action. Remember to ``jewel run`` first so that the network is up.
+The ``do.py`` module is the user interface to this whole thing. It uses a nifty library called ``simple-term-menu``, and the selected options appear in the code as indexes that we can use to take appropriate action. Remember to ``run.sh`` first so that the network is up.
 
 ## Automation
 
-Spinning up a network in various configurations would be time-consuming and error prone, so we use bash scripts to do it. Specifically, ``init.sh`` creates the network in the ``live`` directory by simply copying over files into distinct folders representing nodes or machines, and ``run.sh`` spins everything up once deployed. ``jewel init`` and ``jewel run`` simply delegate to these shell scripts.
+Spinning up a network in various configurations would be time-consuming and error prone, so we use bash scripts to do it. Specifically, ``init.sh`` creates the network in the ``live`` directory by simply copying over files into distinct folders representing nodes or machines, and ``run.sh`` spins everything up once deployed.
 
 # Supporting this Project
 
--- jewel/jewel.py
+++ jewel/jewel.py
@@ -1,40 +1,120 @@
 #!/usr/bin/env python
-import click
-import subprocess
-from .do import do
+import Pyro5.api
+from simple_term_menu import TerminalMenu
+from .networking import discover_peers
+from sys import exit
+import re
+from time import sleep
 
 
-@click.command(
-    help=(
-        "Initialize P2P network at the path `live/`.\n\n"
-        "This must be run while in the repo root folder."
-    )
-)
-@click.argument("n", required=True)
-def init(n):
-    subprocess.call(f"./init.sh {n}", shell=True)
+def error(message):
+    print()
+    print(message)
+    print()
+    sleep(0.5)
 
 
-@click.command(
-    help=(
-        "Spin up the P2P network at the path `live/`.\n\n"
-        "N is the number of peers to spin up.\n\n"
-        "This must be run while in the repo root folder."
-    )
-)
-@click.argument("n", required=True)
-def run(n):
-    subprocess.call(f"./run.sh {n}", shell=True)
+def _extract_number(str):
+    return re.findall(r'\d+', str)[0]
 
 
-@click.group(help="Jewel - prototype P2P storage schemes.")
+def _show_peer_menu():
+    peers = discover_peers()
+    peers = list(peers.keys())
+    peer_options = [f"[{_extract_number(p)}] {p}" for p in peers]
+    menu = TerminalMenu(peer_options, title="Which peer?")
+    peer_index = menu.show()
+    peer_name = peers[peer_index]
+    return peer_name
+
+
+def _show_file_menu(peer):
+    files = peer.dir()
+    if not files:
+        return None
+    menu = TerminalMenu(files, title="Which file?")
+    file_index = menu.show()
+    filename = files[file_index]
+    return filename
+
+
+def dir():
+    """ List files on a selected peer. """
+    peer_name = _show_peer_menu()
+    with Pyro5.api.Proxy(f"PYRONAME:{peer_name}") as peer:
+        print(peer.dir())
+
+
+def store_file():
+    """ Have a peer request to store a file. """
+    peer_name = _show_peer_menu()
+    with Pyro5.api.Proxy(f"PYRONAME:{peer_name}") as peer:
+        filename = _show_file_menu(peer)
+        if filename:
+            print(f"OK. {peer_name} is requesting to store {filename} ...\n")
+            peer.request_to_store(filename)
+        else:
+            error(f"No files present on {peer_name}!")
+
+
+def delete_file():
+    """ Ask a peer to delete a file. """
+    peer_name = _show_peer_menu()
+    with Pyro5.api.Proxy(f"PYRONAME:{peer_name}") as peer:
+        filename = _show_file_menu(peer)
+        if filename:
+            peer.delete(filename)
+        else:
+            error(f"No files present on {peer_name}!")
+
+
+def get_file():
+    """ Have a peer request to get a file. """
+    peer_name = _show_peer_menu()
+    with Pyro5.api.Proxy(f"PYRONAME:{peer_name}") as peer:
+        filename = input("What file would you like to get? ").strip()
+        print(f"OK. {peer_name} is requesting to get {filename} ...\n")
+        peer.request_to_get(filename)
+
+
+def show_help():
+    info = ["[d] dir - show files stored on a peer",
+            "[s] Store a file - instruct a peer to request to store a file on the network",
+            "[x] Delete a file - remove a file from a specific peer",
+            "[g] get a file - instruct a peer to request a file stored on the network",
+            "[?] help - show this menu",
+            "[q] quit - exit to the shell"]
+    print("\n".join(info))
+
+
+def main_menu():
+    options = ["[d] dir",
+               "[s] Store a file",
+               "[x] Delete a file",
+               "[g] get a file",
+               "[?] help",
+               "[q] quit"]
+    menu = TerminalMenu(options, title="What would you like to do?")
+    selected_index = menu.show()
+    if selected_index == 0:
+        dir()
+    elif selected_index == 1:
+        store_file()
+    elif selected_index == 2:
+        delete_file()
+    elif selected_index == 3:
+        get_file()
+    elif selected_index == 4:
+        show_help()
+    elif selected_index == 5:
+        exit(0)
+    else:
+        exit(0)
+
+
 def main():
-    pass
-
-
-main.add_command(init)
-main.add_command(run)
-main.add_command(do)
+    while True:
+        main_menu()
 
 
 if __name__ == '__main__':
