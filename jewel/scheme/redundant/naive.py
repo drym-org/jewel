1368e2212ec53cc1b8ffbbb93170abab982f39b2
Siddhartha <sid@countvajhula.com>
2023-06-20 21:28:42 -0700

    implement block name handshake for naive duplication scheme too

--- jewel/scheme/redundant/naive.py
+++ jewel/scheme/redundant/naive.py
@@ -7,7 +7,7 @@ from .base import RedundantStorageScheme
 from ...striping import stripe_blocks
 from ...block import make_block
 from ...metadata import make_metadata
-from ...networking import peers_available_to_host, hosting_peers
+from ...networking import peers_available_to_host, hosting_peers, block_name_for_file
 from ...file import write_file
 
 
@@ -76,13 +76,16 @@ class NaiveDuplication(RedundantStorageScheme):
     def get(self, filename):
         """ The main entry point to get a file that was stored using this
         scheme. """
-        peers = hosting_peers(filename)
+        block_name = block_name_for_file(filename)
+        peers = hosting_peers(block_name)
         # TODO: when peer metadata is added, we can select the
         # "best" peer here instead of a random peer
         chosen_peer = choice(peers)
         with Pyro5.api.Proxy(chosen_peer) as peer:
             # TODO: need to also retrieve the metadata
             # to compare the checksum
-            file_contents = peer.retrieve(filename)
+            file_contents = peer.retrieve(block_name)
             decoded = base64.decodebytes(bytes(file_contents['data'], 'utf-8'))
+            # write it with the original filename
+            # instead of the block name
             write_file(filename, decoded)
